8994d937ed9a8596f36fd7ead5d7f228f9143e9d
Upasana <112039921+Igdtuwresource@users.noreply.github.com>
2022-11-16 20:02:07 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,2 @@
+# google sign in
+ 
---
+++ src/firebase.js
@@ -0,0 +1,18 @@
+
+import { initializeApp } from "firebase/app";
+import {getAuth} from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBcPRIgRe3xvL0ezvq848xlNOAWJqsaxuM",
+  authDomain: "igdtuw-resource-auth.firebaseapp.com",
+  projectId: "igdtuw-resource-auth",
+  storageBucket: "igdtuw-resource-auth.appspot.com",
+  messagingSenderId: "868177069586",
+  appId: "1:868177069586:web:5c90d5efe5d01829bbfacb",
+  measurementId: "G-E35LWGSCWC"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const auth=getAuth(app);
+export default app;
