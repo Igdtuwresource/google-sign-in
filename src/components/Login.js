140e0c067162e5d00a13889f041ef4a49ea74356
Upasana <112039921+Igdtuwresource@users.noreply.github.com>
2022-11-18 21:21:29 +0530

    Update Login.js

--- src/components/Login.js
+++ src/components/Login.js
@@ -36,7 +36,7 @@ const Login = () => {
   return (
     <>
       <div className="p-4 box">
-        <h2 className="mb-3">ALL THE BEST</h2>
+        <h2 className="mb-3">LOGIN TO YOUR ACCOUNT</h2>
         {error && <Alert variant="danger">{error}</Alert>}
         <Form onSubmit={handleSubmit}>
           <Form.Group className="mb-3" controlId="formBasicEmail">
